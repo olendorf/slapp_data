f079637ba07c5e1bdbfc84318785864bb38ca16f
Robert Olendorf <drolendorf@gmail.com>
2024-07-23 15:51:15 +0000

    cache

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,47 @@ Bundler.require(*Rails.groups)
 module SlappData
   # The main config for the application
   class Application < Rails::Application
+    def set_aws_managed_secrets
+      # secret name created in aws secret manager
+      if ENV['RAISL_ENV']
+        secret_name = "#{ENV['RAILS_ENV']}/database-1/postgres/postgres"
+      else
+        secret_name = 'development/database-1/postgres/postgres'
+      end
+      # region name
+      region_name = 'us-east-2'
+    
+      client = Aws::SecretsManager::Client.new(region: region_name)
+    
+      begin
+        secret_value = client.get_secret_value(secret_id: secret_name)
+      rescue Aws::SecretsManager::Errors::DecryptionFailure => e
+        raise e
+      rescue Aws::SecretsManager::Errors::InternalServiceError => e
+        raise e
+      rescue Aws::SecretsManager::Errors::InvalidParameterException => e
+        raise e
+      rescue Aws::SecretsManager::Errors::InvalidRequestException => e
+        raise e
+      rescue Aws::SecretsManager::Errors::ResourceNotFoundException => e
+        raise e
+      else
+        if secret_value.secret_string
+          secret_hash = JSON.parse(secret_value.secret_string)
+          ENV['DB_IP'] = secret_hash['host']
+          ENV['DB_USERNAME'] = secret_hash['username']
+          ENV['DB_PASSWORD'] = secret_hash['password']
+        end
+      end
+    end
+    
+    
+    
+    set_aws_managed_secrets
+    
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
