5d3e5d8697e3ebe098b9f70d7139c9d982c0e011
Robert Olendorf <drolendorf@gmail.com>
2024-08-08 17:50:48 +0000

    still fixing

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,14 +1,19 @@
 sudo chown -R ubuntu:ubuntu /var/www/html/slapp_data
 
-cd /var/www/html/slapp_data
 
-sudo apt install libpq-dev -y
-sudo apt update
-sudo apt install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev -y libffi-dev libgdbm-dev
+
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+
+# sudo apt-get -y install rbenv ruby-build
+
+if command_exists rbenv
+then
+    echo 'rbenv found, no action needed'
+else
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+    echo 'eval "$(rbenv init -)"' >> ~/.profile
+    source /home/ubuntu/.profile
+fi
 
 rbenv install 3.2.2
 rbenv global 3.2.2
@@ -17,5 +22,8 @@ echo "gem: --no-document" > ~/.gemrc
 # sudo gem install bundler
 
 ruby -v
+
+
+cd /var/www/html/slapp_data
 
 bundle install
