89c888cb560c560b9c985e4bea04f49678fab791
Robert Olendorf <drolendorf@gmail.com>
2024-08-21 14:13:11 +0000

    fixed failing test

--- spec/models/abstract_web_object_spec.rb
+++ spec/models/abstract_web_object_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe AbstractWebObject, type: :model do
   it { expect(AbstractWebObject).to be_actable }
   it {
     should belong_to(:user)
-      .dependent(:destroy)
       .touch(true)
       .required(false)
   }
