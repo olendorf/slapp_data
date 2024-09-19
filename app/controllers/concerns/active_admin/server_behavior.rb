1e4543a6f09f7dc4877990b07da6092014bccaee
Robert Olendorf <drolendorf@gmail.com>
2024-09-19 18:09:43 +0000

    fixing admin pages

--- app/controllers/concerns/active_admin/server_behavior.rb
+++ app/controllers/concerns/active_admin/server_behavior.rb
@@ -9,31 +9,36 @@ module ActiveAdmin
     def self.included(base)
       base.controller do
         def update
-          if params['rezzable_server']['inventories_attributes']
-            InventorySlRequest.batch_destroy(
-              extract_deleted_inventories(params.to_unsafe_h)
+          begin
+            if params['rezzable_server']['inventories_attributes']
+              InventorySlRequest.batch_destroy(
+                extract_deleted_inventories(params.to_unsafe_h)
+              )
+            end
+            RezzableSlRequest.update_web_object!(
+              resource,
+              params[resource.class.name.underscore.gsub('/', '_')]
             )
+            super
+          rescue RestClient::ExceptionWithResponse => e
+            flash[:error] = t('active_admin.web_object.update.failure',
+                              message: e.response)
+            parts = request.url.split('/')[3, 2]
+            redirect_back(fallback_location: send("#{parts.first}_servers_path"))
           end
-          RezzableSlRequest.update_web_object!(
-            resource,
-            params[resource.class.name.underscore.gsub('/', '_')]
-          )
-          super
-        rescue RestClient::ExceptionWithResponse => e
-          flash[:error] = t('active_admin.web_object.update.failure',
-                            message: e.response)
-          parts = request.url.split('/')[3, 2]
-          redirect_back(fallback_location: send("#{parts.first}_servers_path"))
         end
 
         def destroy
-          RezzableSlRequest.derez_web_object!(resource)
-          super
-        rescue RestClient::ExceptionWithResponse => e
-          flash[:error] = t('active_admin.web_object.destroy.failure',
-                            message: e.response)
-          parts = request.url.split('/')[3, 2]
-          redirect_back(fallback_location: send("#{parts.first}_servers_path"))
+          begin
+            RezzableSlRequest.derez_web_object!(resource)
+            super
+          rescue RestClient::ExceptionWithResponse => e
+            flash[:error] = t('active_admin.web_object.destroy.failure',
+                              message: e.response)
+            parts = request.url.split('/')[3, 2]
+            redirect_back(fallback_location: send("#{parts.first}_servers_path"))
+            super
+          end
         end
 
         def extract_deleted_inventories(params)
