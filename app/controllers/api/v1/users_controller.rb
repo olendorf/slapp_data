12fe08294804fec8d28c4cedaf7da4be77a561fe
Robert Olendorf <drolendorf@gmail.com>
2024-10-28 19:39:39 +0000

    fixed something

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,8 @@ module Api
         @user.save!
         
         response_data = @user.attributes
-        response_data[:expiration_date] = response_data[:expiration_date].strftime('%b %d, %Y %I:%M %p')
+        puts response_data
+        response_data[:expiration_date] = response_data["expiration_date"].strftime('%b %d, %Y %I:%M %p')
 
         render json: {
           message: I18n.t('api.user.create.success', url: Settings.site_url),
