ab2b0578ba44f1c317eaba38a6f5c65950bb3b16
Robert Olendorf <drolendorf@gmail.com>
2024-10-29 15:52:50 +0000

    tweaked data sent in show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -30,6 +30,10 @@ module Api
             avatar_name: @user.avatar_name,
             avatar_key: @user.avatar_key,
             role: @user.role,
+            expiration_date: @user.expiration_date,
+            account_level: @user.account_level,
+            object_weight: @user.web_object_weight,
+            object_count: @user.web_object_count,
             http_status: 'OK',
             payment_schedule: User.payment_schedule
           }
