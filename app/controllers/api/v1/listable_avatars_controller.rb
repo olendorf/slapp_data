70d49a2e0d5f967c9b2b232278ff25094ddba041
Robert Olendorf <drolendorf@gmail.com>
2024-12-10 17:19:11 +0000

    added all option to listable avatar indexing

--- app/controllers/api/v1/listable_avatars_controller.rb
+++ app/controllers/api/v1/listable_avatars_controller.rb
@@ -6,12 +6,17 @@ class Api::V1::ListableAvatarsController < Api::V1::ApiController
   
   def index
     authorize [:api, :v1, @requesting_object.actable]
-    
-    page = @requesting_object.actable.send(params['list_name'].to_sym)
-                  .page(params['listable_avatar_page']).per(9)
-    page = page.collect { |a| {avatar_key: a.avatar_key, avatar_name: a.avatar_name} }
-    
-    data = {params['list_name'] => page }
+    data = ""
+    if params['listable_avatar_page'] ||= 'all'
+      data = @requesting_object.actable.send(params['list_name'].to_sym)
+      data = data.collect { |a| {avatar_key: a.avatar_key, avatar_name: a.avatar_name} }
+    else
+      params['listable_avatar_page'] ||= 1
+      page = @requesting_object.actable.send(params['list_name'].to_sym)
+                    .page(params['listable_avatar_page']).per(9)
+      page = page.collect { |a| {avatar_key: a.avatar_key, avatar_name: a.avatar_name} }
+      data = {params['list_name'] => page }
+    end
     
     render json: {message: 'OK', data: data}, status: :ok
   end
