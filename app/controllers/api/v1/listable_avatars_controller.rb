e2134228d172227b10c948e84e13dde7a6617363
Robert Olendorf <drolendorf@gmail.com>
2024-12-10 19:51:44 +0000

    tweak output

--- app/controllers/api/v1/listable_avatars_controller.rb
+++ app/controllers/api/v1/listable_avatars_controller.rb
@@ -9,13 +9,13 @@ class Api::V1::ListableAvatarsController < Api::V1::ApiController
     data = ""
     if params['listable_avatar_page'] ||= 'all'
       page = @requesting_object.actable.send(params['list_name'].to_sym)
-      page = page.collect { |a| {avatar_key: a.avatar_key, avatar_name: a.avatar_name} }
+      page = page.collect { |a| a.avatar_name }
       data = {params['list_name'] => page}
     else
       params['listable_avatar_page'] ||= 1
       page = @requesting_object.actable.send(params['list_name'].to_sym)
                     .page(params['listable_avatar_page']).per(9)
-      page = page.collect { |a| {avatar_key: a.avatar_key, avatar_name: a.avatar_name} }
+      page = page.collect { |a| a.avatar_name }
       data = {params['list_name'] => page }
     end
     
