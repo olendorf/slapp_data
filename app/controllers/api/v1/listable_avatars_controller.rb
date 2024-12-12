cf71e844099431ac1997efdbd95f86543972cb39
Robert Olendorf <drolendorf@gmail.com>
2024-12-12 17:37:45 +0000

    fixed admin thing

--- app/controllers/api/v1/listable_avatars_controller.rb
+++ app/controllers/api/v1/listable_avatars_controller.rb
@@ -7,9 +7,12 @@ class Api::V1::ListableAvatarsController < Api::V1::ApiController
   def index
     authorize [:api, :v1, @requesting_object.actable]
     data = ""
+    puts params
     if params['listable_avatar_page'] == 'all'
       page = @requesting_object.actable.send(params['list_name'].to_sym)
+      puts page
       page = page.collect { |a| a.avatar_name }
+      puts page
       data = {params['list_name'] => page}
     else
       params['listable_avatar_page'] ||= 1
