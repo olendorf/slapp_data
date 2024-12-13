915e891e3b8ad0320df5f64f902056e2096c58d0
Robert Olendorf <drolendorf@gmail.com>
2024-12-13 21:18:55 +0000

    think i fixed detection issue

--- app/models/analyzable/visit.rb
+++ app/models/analyzable/visit.rb
@@ -28,6 +28,7 @@ module Analyzable
       self.avatar_name ||= detection.avatar_name
       self.avatar_key ||= detection.avatar_key
       self.duration = detection.created_at - self.created_at
+      logger.debug "self created at: #{self.created_at}\ndetecte created_at: #{detection.created_at}\nduration: #{self.duration}"
       self.save
     end
   end
