8d44f2b26d942ad72b187a8f9708a9aff894e125
Robert Olendorf <drolendorf@gmail.com>
2025-01-14 17:30:11 +0000

    donation box admin page show started

--- app/models/rezzable/donation_box.rb
+++ app/models/rezzable/donation_box.rb
@@ -8,6 +8,16 @@ class Rezzable::DonationBox < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     %w[id id_value]
   end
+  
+  def biggest_donor
+    data = self.transactions.group(:target_name, :target_key).sum(:amount).sort_by { |_k, v| v }.last
+    {target_name: data.first.first, target_key: data.first.last, amount: data.last}
+  end
+  
+  def donors
+    data = self.transactions.group(:target_name, :target_key).sum(:amount).sort_by { |_k, v| -v }
+    data.collect { |k, v| {target_name: k.first, target_key: k.last, amount: v}}
+  end
 
   def response_data
       {
--- spec/models/rezzable/donation_box_spec.rb
+++ spec/models/rezzable/donation_box_spec.rb
@@ -2,4 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Rezzable::DonationBox, type: :model do
   it_behaves_like 'a rezzable object', :donation_box, 1
+  
+  let(:user) { FactoryBot.create :user }
+  let(:donation_box) do
+    donation_box = FactoryBot.build :donation_box
+    user.web_objects << donation_box
+    5.times do |i|
+      donation_box.transactions << FactoryBot.build(
+                                    :donation, 
+                                    target_name: "User_#{i} Resident",
+                                    target_key: SecureRandom.uuid,
+                                    user_id: user.id,
+                                    amount: 200)
+    end
+    av_key = SecureRandom.uuid
+    3.times do
+      donation_box.transactions << FactoryBot.build(
+                                    :donation,
+                                    target_name: "Big Spender",
+                                    target_key: av_key,
+                                    user_id: user.id,
+                                    amount: 300)
+    end
+    donation_box
+  end
+  
+  describe '#biggest_donor' do
+    it 'should return the correct data' do 
+      expect(donation_box.biggest_donor).to include(target_name: 'Big Spender', amount: 900)
+    end
+  end
+  
+  describe '#donors' do 
+    it 'should return the correct data' do 
+      puts donation_box.donors
+      expect(donation_box.donors.size).to eq 6
+    end
+  end
 end
