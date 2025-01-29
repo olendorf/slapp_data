ee582425a1975a78ccd0281f92eb70eb9e1f106c
Robert Olendorf <drolendorf@gmail.com>
2025-01-29 15:40:29 +0000

    fixed somethign?

--- app/controllers/concerns/api/exception_handler.rb
+++ app/controllers/concerns/api/exception_handler.rb
@@ -14,7 +14,7 @@ module Api
 
       rescue_from Exception do |e|
         Rails.logger.info e.message
-        Rails.logger.info e.backtrace
+        Rails.logger.info e.backtrace.join($/)
         json_response({ message: e.message }, :internal_server_error)
       end
 
